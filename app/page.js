009862d81bf004a0237d61bbef1c8b22114e65bd
paulocs5 <paulocriollo617@gmail.com>
2024-08-27 08:16:48 -0400

    first version

--- app/page.js
+++ app/page.js
@@ -1,59 +1,87 @@
 import Image from "next/image";
 import { useState } from "react";
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 
 export default function Home() {
   const [messages, setMessages] = useState([
     {
-      role:"assistant",
-      constent: "Hi! I am the Rate My Professor support assistant. How can I help you today"
-    }
-  ])
+      role: "assistant",
+      content: "Hi! I am the Rate My Professor support assistant. How can I help you today?",
+    },
+  ]);
 
-  const [message, setMessage] =  useState('')
-    const sendMessage = async () => {
-      setMessages((messages) => [
-        ...messages,
-        {role: "user", content: message},
-        {role: "assistant", content:''},
-      ])
-      setMessage('')  
+  const [message, setMessage] = useState('');
 
-      const response = fetch('/api/chat', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          },
-          body: JSON.stringify([ ...messages, {role: 'user', content: message}]),
-        }).then(async (res) => {
-          const reader = res.body.getReader()
-          const decoder = new TextDecoder()
+  const sendMessage = async () => {
+    setMessages((messages) => [
+      ...messages,
+      { role: "user", content: message },
+      { role: "assistant", content: '' },
+    ]);
+    setMessage('');
 
-          let result = ''
-          return reader.read().then(function processText({done, value}){
-            if (done){
-              return result
-            }
-            const text = decoder.decode(value || new Uint8Array(), {stream:true})
-            setMessages(messages) => { 
-              let lastMessage = messages(messages.length - 1)
-              let otherMessages = messages.slice(0, messages.length - 1)
-              return [
-                ...otherMessages,
-                {...lastMessage, content: lastMessage.content + text},
-              ] 
-            })
-            return reade.read().then(processText)
+    const response = fetch('/api/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify([...messages, { role: 'user', content: message }]),
+    }).then(async (res) => {
+      const reader = res.body.getReader();
+      const decoder = new TextDecoder();
+
+      let result = '';
+      return reader.read().then(function processText({ done, value }) {
+        if (done) {
+          return result;
+        }
+        const text = decoder.decode(value || new Uint8Array(), { stream: true });
+        setMessages((messages) => { 
+          let lastMessage = messages[messages.length - 1];
+          let otherMessages = messages.slice(0, messages.length - 1);
+          return [
+            ...otherMessages,
+            { ...lastMessage, content: lastMessage.content + text },
+          ];
+        });
+        return reader.read().then(processText);
+      });
+    });
+  };
+
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack 
+        direction="column"
+        width="500px"
+        height="800px"
+        border="1px solidblack"
+        p={2}
+        spacing={3}
+      > 
+        {messages.map((mesage, index) => {
+            <Box
+              key={index}
+              display="flex"
+              justifyContent={
+
+              }
+            >
+
+              
+            </Box>
           })
-        })
-    }
-      return ( 
-        <Box 
-          width="100vw"
-          height="100vh"
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-        > </Box>
+        }
+
+      </Stack>
+  
+    </Box>
+  );
 }
